1b332024274787086eff4281a2f3b1a65687fd2b
Ezabulbary <ezabul.bary@gmail.com>
2022-06-30 12:47:26 +0600

    layout component added

---
+++ src/layout/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <section>
+            <div class="navbar bg-base-100">
+                <div class="navbar-start">
+                    <a class="btn btn-ghost normal-case text-xl">Power Hack</a>
+                </div>
+                <div class="navbar-end">
+                    <a class="btn">Get started</a>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Header;
